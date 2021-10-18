34b333156872d01122dc1c72507f6f0411039295
Teodor Wolski <teodorwolski88@gmail.com>
2021-10-18 17:39:18 +0200

    I don't know what I am doing

--- client/src/components/organisms/ChatView/ChatView.js
+++ client/src/components/organisms/ChatView/ChatView.js
@@ -3,25 +3,9 @@ import PropTypes from "prop-types";
 import Sidebar from "components/organisms/Sidebar/Sidebar";
 import TeamChannelList from "components/molecules/TeamChannelList/TeamChannelList";
 import { ChannelList, useChatContext, Chat } from "stream-chat-react";
-import { StreamChat } from "stream-chat";
 import Cookies from "universal-cookie";
-import { useHistory } from "react";
 
-const ApiKey = process.env.API_KEY;
-
-// const cookies = new Cookies();
-
-// const authToken = cookies.get("token");
-
-const client = StreamChat.getInstance(ApiKey);
-
-// if (authToken) {
-//   client.connectUser({
-
-//   })
-// }
-
-const ChatView = ({ children }) => (
+const ChatView = ({ children, client }) => (
   <Chat client={client}>
     <Sidebar />
     {children}
--- client/src/views/Root.js
+++ client/src/views/Root.js
@@ -1,11 +1,19 @@
 import React from "react";
 import MainTemplate from "components/templates/MainTemplate/MainTemplate";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import PrivateRoute from "providers/PrivateRoute";
 import ChatView from "components/organisms/ChatView/ChatView";
 import { routes } from "routes";
 import LoginView from "views/AuthViews/LoginView/LoginView";
 import RegisterView from "views/AuthViews/RegisterView/RegisterView";
+import { StreamChat } from "stream-chat";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+
+const authToken = cookies.get("token");
+
+const ApiKey = process.env.API_KEY;
+
+const client = StreamChat.getInstance(ApiKey);
 
 const Root = () => (
   <Router>
@@ -13,9 +21,9 @@ const Root = () => (
       <MainTemplate>
         <Route exact path={routes.login} component={LoginView} />
         <Route exact path={routes.register} component={RegisterView} />
-        <PrivateRoute exact path={routes.home}>
-          <ChatView />
-        </PrivateRoute>
+        <Route exact path={routes.home}>
+          <ChatView client={client} />
+        </Route>
       </MainTemplate>
     </Switch>
   </Router>
