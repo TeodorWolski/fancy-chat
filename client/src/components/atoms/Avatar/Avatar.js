a0bcf64c5512d597241a758951f52119ae8c86b3
Teodor Wolski <teodorwolski88@gmail.com>
2021-10-26 19:53:22 +0200

    Converting px into rem

--- client/src/components/atoms/Avatar/Avatar.js
+++ client/src/components/atoms/Avatar/Avatar.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 
 const Avatar = styled.div`
   border: 1px solid ${({ theme }) => theme.colors.moreMint};
-  border-radius: 50px;
+  border-radius: 5rem;
   background: url(https://avatars.dicebear.com/api/miniavs/:seed.svg) no-repeat;
-  height: 60px;
-  width: 60px;
+  height: 6rem;
+  width: 6rem;
   position: relative;
   top: 1rem;
   left: 0.75rem;
