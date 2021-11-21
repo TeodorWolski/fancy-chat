5df8ab3d8db2fdb6fe38fb245531f830bcc01113
Teodor Wolski <teodorwolski88@gmail.com>
2021-11-21 21:31:59 +0100

    Fixing route bug and evolving the ChatInput component

--- client/src/components/molecules/ChatInput/ChatInput.js
+++ client/src/components/molecules/ChatInput/ChatInput.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Input } from "./ChatInput.styles";
 
-const ChatInput = () => <Input placeholder="enter your message" />;
+const ChatInput = () => {
+  const messageRef = useRef(null);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    alert("message sent");
+    messageRef.current.value = "";
+  };
+
+  return (
+    <form>
+      <Input placeholder="enter your message" ref={messageRef} />
+      <button hidden type="submit" onClick={sendMessage}>
+        SEND
+      </button>
+    </form>
+  );
+};
 
 export default ChatInput;
