89ccc3526e7d0b9c7b537911111acb63bf663f86
Teodor Wolski <teodorwolski88@gmail.com>
2021-11-24 21:04:01 +0100

    Message sending logic v1

--- client/src/components/molecules/ChatInput/ChatInput.js
+++ client/src/components/molecules/ChatInput/ChatInput.js
@@ -1,11 +1,22 @@
 import React, { useRef } from "react";
 import { Input } from "./ChatInput.styles";
+import { useParams } from "react-router-dom";
+import { db } from "../../../firebase";
+import { collection, addDoc } from "firebase/firestore";
+import firebase from "firebase/compat/app";
 
 const ChatInput = () => {
   const messageRef = useRef(null);
+  const { channelName } = useParams();
 
   const sendMessage = (e) => {
     e.preventDefault();
+
+    db.collection("rooms").doc(channelName).collection("messages").add({
+      message: messageRef.current.value,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
     alert("message sent");
     messageRef.current.value = "";
   };
